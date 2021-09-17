d5977e0e04749a6f6bf3737a6bf4a84ff55cb426
Ivanna Parfeniuk <1998ivankaa@gmail.com>
2021-09-17 10:55:25 +0300

    ch8

--- config/settings.py
+++ config/settings.py
@@ -1,17 +1,13 @@
 import os
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=1^bv_am36())e&7r+t43%g+cj-^^uom8f+m%kg@a(np4x^_*m'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
 ALLOWED_HOSTS = []
 
